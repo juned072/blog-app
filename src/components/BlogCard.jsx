4119aefa9965b2be6d17d1f4beaf47f35ac85233
juned072 <junedansari9172@gmail.com>
2024-08-03 11:10:07 +0530

    changed a card background color and border

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const BlogCard = ({ blog }) => {
   return (
-    <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl my-6">
+    <div className="max-w-md mx-auto bg-gray-800 border border-gray-600 rounded-xl shadow-md overflow-hidden md:max-w-2xl my-6">
       <div className="md:flex">
         <div className="md:shrink-0">
           <img
@@ -15,19 +15,19 @@ const BlogCard = ({ blog }) => {
           <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
             {blog.author}
           </div>
-          <h2 className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+          <h2 className="block mt-1 text-lg leading-tight font-medium text-gray-200 hover:underline">
             {blog.title}
           </h2>
-          <p className="mt-2 text-gray-500">{blog.description}</p>
+          <p className="mt-2 text-gray-400">{blog.description}</p>
           <div className="mt-4">
-            <span className="text-gray-400 text-sm">
+            <span className="text-gray-500 text-sm">
               Published on {blog.date}
             </span>
           </div>
           <div className="mt-4">
             <a
               href=""
-              className="text-indigo-600 hover:text-indigo-900 font-semibold"
+              className="text-indigo-500 hover:text-indigo-700 font-semibold"
             >
               Read More
             </a>
