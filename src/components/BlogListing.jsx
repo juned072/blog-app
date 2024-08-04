0cf3ed09e40fb2a09e7247799f710b46c7e32c67
juned072 <junedansari9172@gmail.com>
2024-08-04 17:33:47 +0530

    Display No Blogs! message when there are no blogs to display

--- src/components/BlogListing.jsx
+++ src/components/BlogListing.jsx
@@ -9,9 +9,17 @@ const BlogListing = ({ isHome }) => {
       <h2 className="text-center font-bold text-2xl text-gray-200">
         {isHome ? "Latest Blog" : "All Blog"}
       </h2>
-      {blogs.map((blog, index) => (
-        <BlogCard key={index} blog={blog} />
-      ))}
+      {blogs.length === 0 ? (
+        <div className="flex justify-center items-center min-h-80">
+          <p className="font-semibold text-gray-300">No Blogs!</p>
+        </div>
+      ) : (
+        <>
+          {blogs.map((blog, index) => (
+            <BlogCard key={index} blog={blog} />
+          ))}
+        </>
+      )}
     </div>
   );
 };
