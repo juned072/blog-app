bbf258f28d03dce86a2ee336696feeb94476d5e4
juned072 <junedansari9172@gmail.com>
2024-08-04 17:39:36 +0530

    Add ContactUs component in a centered layout below the blog listing

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import BlogListing from "../components/BlogListing";
 import HeroSection from "../components/HeroSection";
-
+import ContactUs from "../pages/ContactUs";
 const Home = () => {
   return (
     <div>
       <HeroSection />
       <div className="p-4">
-        <BlogListing isHome={true} />
+        <div>
+          <BlogListing isHome={true} />
+        </div>
+        <div className="max-w-screen-sm m-auto">
+          <ContactUs />
+        </div>
       </div>
     </div>
   );
