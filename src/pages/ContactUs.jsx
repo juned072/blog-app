75b0450d65fa8a4544ab695147f9ba5857da7683
juned072 <junedansari9172@gmail.com>
2024-08-04 17:28:03 +0530

    Logic implemented in the from

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -1,14 +1,46 @@
+import { useState } from "react";
+
 const ContactUs = () => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Form data submitted:", formData);
+    setFormData({
+      name: "",
+      email: "",
+      subject: "",
+      message: "",
+    });
+  };
+
   return (
     <div className="max-w-screen-md mx-auto p-4">
       <h1 className="text-2xl font-bold mb-6 text-center p-4">Contact Us</h1>
-      <form className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-gray-800 shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
         <div className="mb-4">
           <label className="block text-sm font-medium mb-1" htmlFor="name">
             Name
           </label>
           <input
             type="text"
+            name="name"
+            id="name"
+            value={formData.name}
+            onChange={handleChange}
             className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:border-indigo-500"
             placeholder="Your name"
           />
@@ -19,6 +51,10 @@ const ContactUs = () => {
           </label>
           <input
             type="email"
+            name="email"
+            id="email"
+            value={formData.email}
+            onChange={handleChange}
             className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:border-indigo-500"
             placeholder="Your email"
           />
@@ -29,6 +65,10 @@ const ContactUs = () => {
           </label>
           <input
             type="text"
+            name="subject"
+            id="subject"
+            value={formData.subject}
+            onChange={handleChange}
             className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:border-indigo-500"
             placeholder="Subject"
           />
@@ -38,6 +78,10 @@ const ContactUs = () => {
             Message
           </label>
           <textarea
+            name="message"
+            id="message"
+            value={formData.message}
+            onChange={handleChange}
             className="w-full px-4 py-2 rounded-lg bg-gray-700 border border-gray-600 focus:outline-none focus:border-indigo-500"
             placeholder="Your message"
             rows="5"
