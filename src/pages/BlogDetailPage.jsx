939d43370b37e209a79c925610c8e1ba12ccbc63
juned072 <junedansari9172@gmail.com>
2024-08-05 21:03:37 +0530

    dynamic options menu for editing and deleting the blog post

--- src/pages/BlogDetailPage.jsx
+++ src/pages/BlogDetailPage.jsx
@@ -1,30 +1,68 @@
-import React, { useContext } from "react";
-import { Link, useParams } from "react-router-dom";
+import React, { useContext, useState, useEffect, useRef } from "react";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import BlogContext from "../context/BlogContext";
 import { IoMdMore } from "react-icons/io";
 import { MdHome } from "react-icons/md";
 
 const BlogDetail = () => {
+  const [showOptions, setShowOptions] = useState(false);
   const { id } = useParams();
-  const { blogs } = useContext(BlogContext);
+  const { blogs, handleDeleteBlog } = useContext(BlogContext);
   const blogPost = blogs.find((post) => post.id === id);
+  const optionsRef = useRef(null);
+  const navigate = useNavigate();
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (optionsRef.current && !optionsRef.current.contains(event.target)) {
+        setShowOptions(false);
+      }
+    };
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  const handleDelete = () => {
+    navigate("/blog");
+    handleDeleteBlog(id);
+  };
   if (!blogPost) {
     return <div className="text-center mt-20 text-xl">Blog post not found</div>;
   }
 
   return (
-    <div className="max-w-screen-lg  mx-auto p-8">
-      <div className="mb-8 flex justify-between items-center">
+    <div className="max-w-screen-lg mx-auto p-8">
+      <div className="mb-8 flex justify-between items-center relative">
         <Link to={"/"}>
           <span className="flex items-center">
             <MdHome className="text-xl" />
             <span className="ml-1">Home</span>
           </span>
         </Link>
-        <span className="text-gray-100 text-2xl cursor-pointer">
+        <span
+          onClick={() => setShowOptions(!showOptions)}
+          className="text-gray-100 text-2xl cursor-pointer"
+        >
           <IoMdMore />
         </span>
+        {showOptions && (
+          <div
+            ref={optionsRef}
+            className="absolute bg-gray-700 text-white shadow-lg rounded-md right-0 top-8 w-48"
+          >
+            <button className="block w-full text-left px-4 py-3 hover:bg-gray-600">
+              Edit
+            </button>
+            <button
+              onClick={handleDelete}
+              className="block w-full text-left px-4 py-3 hover:bg-gray-600"
+            >
+              Delete
+            </button>
+          </div>
+        )}
       </div>
       <div>
         <div className="mb-4">
