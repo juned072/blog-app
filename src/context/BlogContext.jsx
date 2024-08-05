e1833e985a497517190d58b0ed98dd520f3187d8
juned072 <junedansari9172@gmail.com>
2024-08-05 16:05:37 +0530

    all blogs are stored in localstorage

--- src/context/BlogContext.jsx
+++ src/context/BlogContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
 
 const BlogContext = createContext();
 
 export const BlogProvider = ({ children }) => {
-  const [blogs, setBlogs] = useState([]);
+  const [blogs, setBlogs] = useState(() => {
+    const storedBlogs = localStorage.getItem("blogs");
+    return storedBlogs ? JSON.parse(storedBlogs) : [];
+  });
 
   const addBlog = (blog) => {
     setBlogs((prevBlogs) => [...prevBlogs, blog]);
   };
+
+  useEffect(() => {
+    localStorage.setItem("blogs", JSON.stringify(blogs));
+  }, [blogs]);
 
   return (
     <BlogContext.Provider value={{ blogs, addBlog }}>
