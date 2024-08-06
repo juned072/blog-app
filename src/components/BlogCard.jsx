50c3da128b87d6ad1d2977a5ac0ed4e193f07bd5
juned072 <junedansari9172@gmail.com>
2024-08-06 09:28:45 +0530

    Include on image and title link for full blog post

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -4,27 +4,29 @@ import { Link } from "react-router-dom";
 const BlogCard = ({ blog }) => {
   return (
     <div className="max-w-md mx-auto bg-gray-800 border border-gray-600 rounded-xl shadow-md overflow-hidden my-6">
-      <img
-        className="h-48 w-full object-cover"
-        src={blog.imageURL}
-        alt={blog.title}
-      />
-      <div className="p-8">
+      <Link to={`/blog/${blog.id}`}>
+        <img
+          className="h-48 w-full object-cover"
+          src={blog.imageURL}
+          alt={blog.title}
+        />
+      </Link>
+      <div className="p-5">
         <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
           {blog.author}
         </div>
         <h2 className="block mt-1 text-lg leading-tight font-medium text-gray-200 hover:underline">
-          {blog.title}
+          <Link to={`/blog/${blog.id}`}>{blog.title}</Link>
         </h2>
         <p className="mt-2 text-gray-400">
           {blog.description.slice(0, 100)}...
         </p>
-        <div className="mt-4">
+        <div className="mt-2">
           <span className="text-gray-500 text-sm">
             Published on {blog.date}
           </span>
         </div>
-        <div className="mt-4">
+        <div className="mt-2">
           <Link to={`/blog/${blog.id}`}>
             <span className="text-indigo-500 hover:text-indigo-700 font-semibold">
               Read More
